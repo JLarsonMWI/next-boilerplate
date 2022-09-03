f816917550c64f45d75c2c2fdc62872e753f9ba2
John Larson <jl@buildmidwestern.com>
2022-09-03 16:57:43 -0500

    adjusted page text

--- pages/plans.jsx
+++ pages/plans.jsx
@@ -8,7 +8,7 @@ export default function Home() {
 			<main className="flex h-full w-full items-center justify-center bg-th-background-primary p-2 md:p-10">
 				<div className="flex h-full w-full max-w-5xl flex-col items-center justify-start rounded-3xl bg-th-background-secondary p-4 text-center text-th-text-primary">
 					<div className="py-2 text-2xl font-bold">
-						<p>This is a title!</p>
+						<p>This is a title! Page: Plans</p>
 					</div>
 					<div className="py-2 text-lg">
 						<p className="mb-2">This is some normal text</p>

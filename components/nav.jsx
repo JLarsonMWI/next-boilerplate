e52c2c04aa33f8eaf8011587ca59222753f59d09
John Larson <jl@buildmidwestern.com>
2022-09-03 16:50:14 -0500

    fix: fixed routing

--- components/nav.jsx
+++ components/nav.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import ThemeChanger from './ThemeChanger';
+import ThemeChanger from './theme-changer';
 
 export default function Nav() {
 	const [active, setActive] = useState(false);
